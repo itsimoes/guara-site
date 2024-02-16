342038b71784fdbbb87516065faf3509724630a3
Italo Simões <2d.italo@gmail.com>
2024-02-16 10:35:29 -0300

    PortfolioCarousel e PortfolioIndex components MOBILE estilizados

--- src/components/Portfolio/PortfolioIndex.tsx
+++ src/components/Portfolio/PortfolioIndex.tsx
@@ -3,10 +3,10 @@ import PortfolioCarousel from "./PortfolioCarousel";
 import "./style.scss";
 
 //images
-import AtendimentoCard1 from "../../assets/images/atendimento_thumb_1.png"
-import AtendimentoCard2 from "../../assets/images/atendimento_thumb_2.png"
-import AtendimentoCard3 from "../../assets/images/atendimento_thumb_3.png"
-import AtendimentoCard4 from "../../assets/images/atendimento_thumb_4.png"
+import AtendimentoCard1 from "../../assets/images/atendimento_thumb_1.png";
+import AtendimentoCard2 from "../../assets/images/atendimento_thumb_2.png";
+import AtendimentoCard3 from "../../assets/images/atendimento_thumb_3.png";
+import AtendimentoCard4 from "../../assets/images/atendimento_thumb_4.png";
 
 const PortfolioIndex = () => {
   return (
@@ -24,18 +24,40 @@ const PortfolioIndex = () => {
           </div>
         </div>
         <PortfolioCarousel />
+        <a href=" " id="portfolio_projects_button_mobile" className="button banner_button portfolio_button">
+          VEJA MAIS PROJETOS
+        </a>
         <div className="portfolio_subtitle_container">
-            <h3>Atendimentos em Destaque</h3>
-            <a href=" " className="button banner_button portfolio_button_reverse">
-              VEJA MAIS ATENDIMENTOS
-            </a>
-          </div>
-          <div className="portfolio_atendimento_cards_container">
-            <img src={AtendimentoCard1} alt="" className="portfolio_atendimento_card" />
-            <img src={AtendimentoCard2} alt="" className="portfolio_atendimento_card" />
-            <img src={AtendimentoCard3} alt="" className="portfolio_atendimento_card" />
-            <img src={AtendimentoCard4} alt="" className="portfolio_atendimento_card" />
-          </div>
+          <h3>Atendimentos em Destaque</h3>
+          <a href=" " className="button banner_button portfolio_button_reverse">
+            VEJA MAIS ATENDIMENTOS
+          </a>
+        </div>
+        <div className="portfolio_atendimento_cards_container">
+          <img
+            src={AtendimentoCard1}
+            alt=""
+            className="portfolio_atendimento_card"
+          />
+          <img
+            src={AtendimentoCard2}
+            alt=""
+            className="portfolio_atendimento_card"
+          />
+          <img
+            src={AtendimentoCard3}
+            alt=""
+            className="portfolio_atendimento_card"
+          />
+          <img
+            src={AtendimentoCard4}
+            alt=""
+            className="portfolio_atendimento_card"
+          />
+        </div>
+        <a href=" " id="portfolio_button_atendimento_mobile" className="button banner_button portfolio_button_reverse">
+            VEJA MAIS ATENDIMENTOS
+          </a>
       </div>
     </div>
   );
