939e7f1ba48174791fa35a978aeec9e4379709ae
Italo Simões <2d.italo@gmail.com>
2024-02-19 10:25:08 -0300

    About page atualizada para MOBILE e complementada com banner e footer

--- src/Pages/About/AboutPage.tsx
+++ src/Pages/About/AboutPage.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import About from '../../components/About/About'
+import React from "react";
+import About from "../../components/About/About";
+import ContactBannerWhite from "../../components/Banners/ContactBannerWhite";
+import Footer from "../../components/Footer/Footer";
 
 const AboutPage = () => {
   return (
-    <About />
-  )
-}
+    <>
+      <About />
+      <ContactBannerWhite />
+      <Footer />
+    </>
+  );
+};
 
-export default AboutPage
+export default AboutPage;
