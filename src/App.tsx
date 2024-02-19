be3bb5f17cb1ccf3931c230d7ae161851c35e0c8
Italo Simões <2d.italo@gmail.com>
2024-02-19 13:54:08 -0300

    Suporte Page criada

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import Navbar from "./components/Navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import SiteInstitucionalPage from "./Pages/SiteInstitucional/SiteInsitucionalPage";
-import SuporteDeTI from "./components/Services/SuporteDeTI";
 import PortfolioExpanded from "./components/Portfolio/PortfolioExpanded";
 import AboutPage from "./Pages/About/AboutPage";
 import HomePage from "./Pages/Home/HomePage";
+import Suporte from "./Pages/Suporte/Suporte";
 
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/siteinstitucional" element={<SiteInstitucionalPage />} />
-          <Route path="/suporte" element={<SuporteDeTI />} />
+          <Route path="/suporte" element={<Suporte/>} />
           <Route path="/portfolio" element={<PortfolioExpanded />} />
         </Routes>
       </Router>
---
+++ src/Pages/Suporte/Suporte.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import SuporteDeTI from "../../components/Services/SuporteDeTI";
+import ContactBannerWhite from "../../components/Banners/ContactBannerWhite";
+import Footer from "../../components/Footer/Footer";
+
+const Suporte = () => {
+  return (
+    <>
+      <SuporteDeTI />
+      <ContactBannerWhite />
+      <Footer />
+    </>
+  );
+};
+
+export default Suporte;
